491a8620dd0a3759420d7433e860e98d6b52ebf9
juanmesa2097 <juanmesa2097@gmail.com>
2021-02-14 20:11:40 -0500

    chore: allow users to anotate goals

--- src/app/game/pages/battle/battle.page.ts
+++ src/app/game/pages/battle/battle.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { interval } from 'rxjs';
 import { takeWhile } from 'rxjs/operators';
 
@@ -7,21 +8,97 @@ import { takeWhile } from 'rxjs/operators';
   styleUrls: ['./battle.page.scss'],
 })
 export class BattlePage implements OnInit {
+  readonly numberOfClicks = 20;
+  readonly goalsToWin = 3;
+  readonly stageEager = 4000;
+
+  yellowClicks = 0;
+  purpleClicks = 0;
+
+  yellowLife = 100;
+  purpleLife = 100;
+
   yellowScore = 0;
   purpleScore = 0;
 
-  countDown = 5;
+  countDown = 10;
+
   everybodyJoined = false;
 
-  constructor() {}
+  goal = false;
+  winner!: 'yellow' | 'purple';
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     setTimeout(() => {
       this.everybodyJoined = true;
-
-      interval(1000)
-        .pipe(takeWhile(() => this.countDown > 0))
-        .subscribe(() => this.countDown--);
+      this.runCountdown();
     }, 4000);
+  }
+
+  onClickYellow() {
+    this.purpleLife = this.updateTeamLife(this.yellowClicks++);
+
+    if (this.purpleLife === 0) {
+      this.goal = true;
+      this.yellowScore++;
+
+      if (this.yellowScore === this.goalsToWin) {
+        this.endMatch('yellow');
+      } else {
+        this.reset();
+      }
+    }
+  }
+
+  onClickPurple() {
+    this.yellowLife = this.updateTeamLife(this.purpleClicks++);
+
+    if (this.yellowLife === 0) {
+      this.goal = true;
+      this.purpleScore++;
+
+      if (this.purpleScore === this.goalsToWin) {
+        this.endMatch('purple');
+      } else {
+        this.reset();
+      }
+    }
+  }
+
+  private runCountdown(): void {
+    interval(1000)
+      .pipe(takeWhile(() => this.countDown > 0))
+      .subscribe(() => this.countDown--);
+  }
+
+  private updateTeamLife(clicks: number): number {
+    const life = ((this.numberOfClicks - clicks) * 100) / this.numberOfClicks;
+    return life;
+  }
+
+  private reset(): void {
+    this.yellowLife = 100;
+    this.purpleLife = 100;
+    this.yellowClicks = 0;
+    this.purpleClicks = 0;
+
+    setTimeout(() => {
+      this.countDown = 5;
+      this.goal = false;
+      this.runCountdown();
+    }, this.stageEager);
+  }
+
+  private endMatch(winner: 'yellow' | 'purple'): void {
+    setTimeout(() => {
+      this.goal = false;
+      this.winner = winner;
+
+      setTimeout(() => {
+        this.router.navigate(['/']);
+      }, this.stageEager);
+    }, this.stageEager);
   }
 }
