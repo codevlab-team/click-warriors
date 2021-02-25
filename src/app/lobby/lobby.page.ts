6954b2f61c3f16098c7c892e7082d448c0b589c5
juanmesa2097 <juanmesa2097@gmail.com>
2021-02-24 22:15:20 -0500

    chore: update lobby navigation

--- src/app/lobby/lobby.page.ts
+++ src/app/lobby/lobby.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
 import { interval } from 'rxjs';
-import { map, takeWhile } from 'rxjs/operators';
+import { map, takeWhile, tap } from 'rxjs/operators';
 import { Server, ServerTeam, Team } from '../@core/models/server.model';
 import { HttpMethod } from '../@core/services/http-generic';
 import { ServersService } from '../@core/services/servers/servers.service';
@@ -89,39 +89,39 @@ export class LobbyPage implements OnInit {
         map((value) => countdownSeconds - value),
         takeWhile((value) => value >= 0)
       )
-      .subscribe((value) => {
+      .subscribe(async (value) => {
         const minutes = Math.floor(value / 60);
         const seconds = value - minutes * 60;
         this.displayTime = `${minutes}:${seconds.toString().padStart(2, '0')}`;
-
+        console.log(value);
         if (value === 0) {
           if (
             this.userInTeamYellow === false &&
             this.userInTeamPurple === false
           ) {
-            this.joinAutomatically();
-          } else {
-            this.router.navigate(['/game/battle', this.serverId]);
+            await this.joinAutomatically();
           }
+
+          this.router.navigate(['/game/battle', this.serverId]);
         }
       });
   }
 
-  private joinAutomatically(): void {
+  private async joinAutomatically(): Promise<void> {
     const user = this.usersService.user;
     if (user) {
       const playersInYellow = this.teamYellow.length;
       const playersInPurple = this.teamPurple.length;
 
       if (playersInYellow < playersInPurple) {
-        this.addPlayerToTeamYellow(user.nickname);
+        await this.addPlayerToTeamYellow(user.nickname);
       } else {
-        this.addPlayerToTeamPurple(user.nickname);
+        await this.addPlayerToTeamPurple(user.nickname);
       }
     }
   }
 
-  private addPlayerToTeamYellow(nickname: string): void {
+  private addPlayerToTeamYellow(nickname: string): Promise<ServerTeam> | null {
     if (
       this.serverId &&
       this.teamYellow.some((p) => p.playerNickname === nickname) === false
@@ -135,24 +135,28 @@ export class LobbyPage implements OnInit {
         ready: false,
       };
 
-      this.serversService
+      return this.serversService
         .update(this.serverId, player, {
           urlPostfix: 'player/yellow',
           method: HttpMethod.PATCH,
           errorMsg:
             'Ocurrió un problema al intentar unir el jugador al equipo amarillo.',
         })
-        .subscribe((res) => {
-          this.userInTeamYellow = true;
-          this.userInTeamPurple = false;
-          this.usersService.patchUser({ team: 'yellow' });
-          this.loaderVisible = false;
-          this.router.navigate(['/game/battle', this.serverId]);
-        });
+        .pipe(
+          tap(() => {
+            this.userInTeamYellow = true;
+            this.userInTeamPurple = false;
+            this.usersService.patchUser({ team: 'yellow' });
+            this.loaderVisible = false;
+          })
+        )
+        .toPromise();
     }
+
+    return null;
   }
 
-  private addPlayerToTeamPurple(nickname: string): void {
+  private addPlayerToTeamPurple(nickname: string): Promise<ServerTeam> | null {
     if (
       this.serverId &&
       this.teamPurple.some((p) => p.playerNickname === nickname) === false
@@ -166,20 +170,23 @@ export class LobbyPage implements OnInit {
         ready: false,
       };
 
-      this.serversService
+      return this.serversService
         .update(this.serverId, player, {
           urlPostfix: 'player/purple',
           method: HttpMethod.PATCH,
           errorMsg:
             'Ocurrió un problema al intentar unir el jugador al equipo morado.',
         })
-        .subscribe((res) => {
-          this.userInTeamPurple = true;
-          this.userInTeamYellow = false;
-          this.usersService.patchUser({ team: 'purple' });
-          this.loaderVisible = false;
-          this.router.navigate(['/game/battle', this.serverId]);
-        });
+        .pipe(
+          tap(() => {
+            this.userInTeamPurple = true;
+            this.userInTeamYellow = false;
+            this.usersService.patchUser({ team: 'purple' });
+            this.loaderVisible = false;
+          })
+        )
+        .toPromise();
     }
+    return null;
   }
 }
